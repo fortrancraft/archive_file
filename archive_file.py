d6c7468c73a1adc69e2570b411977763fac16cd2
Jeffrey R. Bell <jeffreybell.ase@gmail.com>
2019-11-10 15:29:21 -0600

    Added gzip archived file compression option

--- archive_file.py
+++ archive_file.py
@@ -2,7 +2,7 @@
 
 __author__      = 'Jeffrey R. Bell'
 __license__     = "GPL-3.0"
-__version__     = "0.0.0"
+__version__     = "0.1.0"
 __status__      = "beta"
 __description__ = 'utility to archive a file by renaming it with a time ' + \
                   'time stamp suffix, and moving it to an archive directory.'
@@ -10,12 +10,12 @@ __email__       = 'fortrancraft@gmail.com',
 __url__         = 'https://github.com/fortrancraft/archive_file',
 
 # Create an archive version of a file by adding a time stamp suffix
-# to the file name, and move it to an archive directory.
-#
-# JRB 2019/09/21
+# to the file name, and move it to an archive directory, compress it, etc...
+
 
 import argparse
 import datetime
+import gzip
 import os
 import shutil
 
@@ -25,16 +25,20 @@ def make_arg_parser():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('input_file',
-        help='Input the file to archive')
+        help='input the file to archive')
 
     parser.add_argument('--da', default='.',
-        help='Input a directory to move the archive file to.')
+        help='input a directory to move the archive file to.')
 
     parser.add_argument('--verbose', '-v', action='count', default=0,
-        help='Set the output printing verbosity level')
+        help='set the output printing verbosity level')
 
     parser.add_argument('--dry-run', action='store_true', default=False,
-        help='Execute the script without moving the input file')
+        help='execute the script without moving the input file')
+
+    parser.add_argument('-z', '--gzip',
+        action='store_true', default=False,
+        help='compress the archived file in place with gzip')
 
     args = parser.parse_args()
 
@@ -43,13 +47,13 @@ def make_arg_parser():
         print('args.da         = {}'.format(args.da))
         print('args.verbose    = {}'.format(args.verbose))
         print('args.dry_run    = {}'.format(args.dry_run))
+        print('args.gzip       = {}'.format(args.gzip))
 
     return args
 
 
 def main():
-    """Archive a file by appending a time stamp to the file's name, and
-       moving it to an archive directory.
+    """Archive a file.
     """
 
     # Prepare for execution.
@@ -80,8 +84,9 @@ def main():
         .strftime('%Y%m%dT%H%M')
 
     # Create the archive file name and path.
-    archive_name = file_name + '__{}{}'.format(tstr, file_ext)
+    archive_name = file_name + '__{}'.format(tstr) + file_ext
     archive_file = os.path.join(args.da, archive_name)
+    archive_file_gz = archive_file + '.gz'
 
     if args.verbose > 0:
         print(' ')
@@ -95,6 +100,8 @@ def main():
             print('tstr        = {}'.format(tstr))
         print('archive_name = {}'.format(archive_name))
         print('archive_file = {}'.format(archive_file))
+        if args.gzip:
+            print('archive_file_gz = {}'.format(archive_file_gz))
 
     # Move the file to the archive directory with the new name.
     if not args.dry_run:
@@ -103,17 +110,41 @@ def main():
             msg = 'achive_file = {} alreadys exists!'.format(archive_file)
             raise FileExistsError(msg)
         else:
+            # Move the file to the archive directory.
             shutil.move(args.input_file, archive_file)
+
+            # Compress the file if requested.
+            if args.gzip:
+                with open(archive_file, 'rb') as f_in:
+                    with gzip.open(archive_file_gz, 'wb') as f_out:
+                        shutil.copyfileobj(f_in, f_out)
+                os.remove(archive_file)
 
     # Check if the file move was successful.
     if args.dry_run:
         pass
     else:
-        if not os.path.isfile(archive_file):
-            msg = 'Failed to archive the file! File move unsuccessful.'
-            raise FileNotFoundError(msg)
+        if args.gzip:
 
-    print('moved {} to {}{}'.format(args.input_file, archive_file, drnote))
+            # Check if the file move and compression worked.
+            if not os.path.isfile(archive_file_gz):
+                msg = 'Failed to archive and compress the file!'
+                raise FileNotFoundError(msg)
+
+        else:
+
+            # Check if the file move worked.
+            if not os.path.isfile(archive_file):
+                msg = 'Failed to archive the file!'
+                raise FileNotFoundError(msg)
+
+
+    if args.gzip:
+        out_fmt = 'moved and compressed {} to {}{}'
+        print(out_fmt.format(args.input_file, archive_file_gz, drnote))
+    else:
+        out_fmt = 'moved {} to {}{}'
+        print(out_fmt.format(args.input_file, archive_file, drnote))
 
 if __name__ == "__main__":
     main()
